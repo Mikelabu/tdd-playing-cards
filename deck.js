3dc0ef305b6db01e585e540a53375330ce773d01
Mikelabu <michael.labu@gmail.com>
2021-10-20 13:49:46 +1300

    Getting a card from the deck

---
+++ deck.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+    getCards: getCards
+  }
+  
+  function getCards (amount) {
+    return "Ace of Spades"
+  }
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,3 +1,12 @@
+const deck = require('../deck') // this is the line to add
+
 test('test setup working', () => {
     expect(true).toBeTruthy()
+  })
+
+  test('returns a card', () => {
+    const amount = 1
+    const expected = "Ace of Spades"
+    const actual = deck.getCards(amount)
+    expect(actual).toBe(expected)
   })
